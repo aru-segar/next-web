1de8d8304efa89a618632db897be6b70111da8d7
aru_segar <arunigagnanasegaran@gmail.com>
2025-07-11 01:16:53 +0530

    real-time search with url params

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,9 +4,10 @@ import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }) {
-  const { query } = await searchParams;
+  const query = (await searchParams).query;
+  const params = { search: query || null};
 
-  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
+const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
   return (
     <>
